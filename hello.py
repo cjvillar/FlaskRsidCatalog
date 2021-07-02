8ca0748712a890949cf4d36a27d8510e14ea2a7e
Chris V <chris@Chriss-MacBook-Air.local>
2021-07-02 12:26:36 -0700

    adding updates

--- hello.py
+++ hello.py
@@ -1,12 +1,16 @@
 from requests.api import get
 from flask_wtf import FlaskForm 
 from flask_bootstrap import Bootstrap
-from flask_sqlalchemy import SQLAlchemy
 from scripts.litvar_api import litvar_url
 from wtforms.validators import InputRequired, Email, Length
 from wtforms import StringField, PasswordField, BooleanField
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask import Flask, render_template, redirect, url_for, request, session
+import sqlite3 as sql
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import backref, relationship
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Table, Column, Integer, ForeignKey
 from flask_login import LoginManager,login_required, UserMixin, login_user, logout_user, current_user
 
 
@@ -20,10 +24,25 @@ db = SQLAlchemy(app)
 login_manager = LoginManager()
 login_manager.init_app(app)
 
+
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer,primary_key=True)
     username = db.Column(db.String(30), unique=True)
     password = db.Column(db.String(80))
+    # variant = db.relationship(variant)
+
+class variant(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    rs_id = db.Column(db.String(20))
+    gene = db.Column(db.String(20))
+    diseases = db.Column(db.String(20))
+    # user_profile = Column('User',db.Integer(), db.ForeignKey('User.variant'))
+
+    def __init__(self, rs_id, gene,diseases):#,user_profile):
+        self.rs_id = rs_id
+        self.gene = gene
+        self.diseases = diseases
+        # self.user_profile = user_profile
 
 @login_manager.user_loader
 def load_user(user_id):
@@ -38,16 +57,6 @@ class RegisterForm(FlaskForm):
     username = StringField('username', validators=[InputRequired(), Length(min=4, max=15)])
     password = PasswordField('password', validators=[InputRequired(), Length(min=8, max=80)])
 
-class variant(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    rs_id = db.Column(db.String(20))
-    gene = db.Column(db.String(20))
-    diseases = db.Column(db.String(20))
-
-    def __init__(self, rs_id, gene,diseases):
-        self.rs_id = rs_id
-        self.gene = gene
-        self.diseases = diseases
 
 @app.route('/')
 def index():
@@ -91,7 +100,7 @@ def login():
 @app.route('/home_page')
 @login_required
 def home():
-    return render_template('home_page.html', logged_in_user = current_user.login_form.username.data)
+    return render_template('home_page.html', logged_in_user = current_user.username)
 
 @app.route('/logout')
 @login_required
@@ -104,9 +113,32 @@ def logout():
 def rsID():
     if request.method == "POST":
         rsid = request.form['rs']
-        return render_template('rsID.html',get_id_response = litvar_url(rsid))
-    else:
-        return render_template('rsID.html')
+        # user_profile= User.user_variant  
+        get_id_response = litvar_url(rsid)
+        new_litvar_info = variant(rs_id=get_id_response[0],gene=get_id_response[3]['name'],diseases=str(get_id_response[1].keys()))#, user_profile=user_profile)
+        rs_check = db.session.query(variant).filter_by(rs_id=rsid)
+        rs = str(rs_check)
+        print(rs.strip('##'))
+        if rsid == rs:
+            return render_template('rsID.html', get_id_response =litvar_url(rsid))
+        # store in database
+        else:
+
+            try:
+                db.session.add(new_litvar_info)
+                # db.session.add(new_gene)
+                # db.session.add(new_diseases)
+                db.session.commit()
+
+            except Exception as error:
+                db.session.rollback()
+                return '<h1>ERROR: {}</h1>'.format(error)
+
+            return render_template('rsID.html', get_id_response =litvar_url(rsid))
+
+
+        #return render_template('rsID.html')
+
     
 if __name__ == '__main__':
     app.run(debug=True)
