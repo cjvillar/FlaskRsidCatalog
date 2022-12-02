e9fdfaaa193d11d7c23738a244929d056cdc1123
Chris Villarreal <chrisvillarreal84@gmail.com>
2022-12-01 18:04:37 -0800

    adding cypress test

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "starting venv and running hello.py"
-. venv/bin/activate
+source ./venv/bin/activate
 export FLASK_APP=hello.py
 export 'PYTHONPATH="${PYTHONPATH}:/Users/chris/flask_project'
 flask run
