589959ac483e97eaa08b1e1d9e87369a48d99cb6
Chris Villarreal <cjvillar@users.noreply.github.com>
2023-02-01 10:32:59 -0800

    fix test

--- cypress/e2e/log_in_tests.cy.js
+++ cypress/e2e/log_in_tests.cy.js
@@ -1,14 +1,10 @@
-
-
-describe('log in with test_user and log out', {
+describe('Attempt log in withinvalid user name and password', {
 },() => {
-  it('passes', () => {
+  it('should error for invalid user', () => {
     cy.visit('/')
-    cy.get(':nth-child(1) > .form-signin > :nth-child(3) > #username').type(Cypress.env('USERNAME'))
-    cy.get(':nth-child(1) > .form-signin > :nth-child(4) > #password').type(Cypress.env('PASSWORD'))
+    cy.get(':nth-child(1) > .form-signin > :nth-child(3) > #username').type(Cypress.env('INVALIDUSERNAME'))
+    cy.get(':nth-child(1) > .form-signin > :nth-child(4) > #password').type(Cypress.env('INVALIDPASSWORD'))
     cy.get(':nth-child(1) > .form-signin > .btn').click()
-    cy.url().should('include', '/home_page')
-    cy.get('.home > h1').should('contain', 'test_user')
-    cy.get(':nth-child(3) > a').click()
+    cy.get('h1').and("have.text", "Invalid username or password")
   })
 })
