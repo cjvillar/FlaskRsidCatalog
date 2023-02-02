e9a1000ef5e9c6e8df65c30130301a957a6f34ae
Chris Villarreal <cjvillar@users.noreply.github.com>
2023-02-02 12:48:58 -0800

    cleanup with black

--- scripts/litvar_api.py
+++ scripts/litvar_api.py
@@ -2,26 +2,25 @@ import json
 import requests
 import argparse
 
+
 def litvar_url(rs):
-    url=("https://www.ncbi.nlm.nih.gov/research/bionlp/litvar/api/v1/entity/litvar/{}%23%23".format(rs))
+    url = "https://www.ncbi.nlm.nih.gov/research/bionlp/litvar/api/v1/entity/litvar/{}%23%23".format(
+        rs
+    )
     response = requests.get(url)
-    try: 
+    try:
         data = response.json()
         rs_id = data["id"]
         gene = data["gene"]
         diseases = data["diseases"]
-        return rs_id,diseases, response,gene, data.keys()
+        return rs_id, diseases, response, gene, data.keys()
     except ValueError:
-         return None
+        return None
 
-        
 
-if __name__=="__main__":
+if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument('rs')
+    parser.add_argument("rs")
     arguments = parser.parse_args()
     rs = arguments.rs
     print(litvar_url(rs))
-    
-    
-    
--- test/test_api.py
+++ test/test_api.py
@@ -2,9 +2,8 @@ from scripts.litvar_api import litvar_url
 
 
 def test_litvar_url():
-    rs = 'rs121913527'
+    rs = "rs121913527"
     test = litvar_url(rs)
     assert test[2].status_code == 200
-    assert test[0] == 'rs121913527##'
-    assert test[3]['name'] == 'KRAS'
-
+    assert test[0] == "rs121913527##"
+    assert test[3]["name"] == "KRAS"
